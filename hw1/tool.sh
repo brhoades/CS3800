dd4a35a2156858f24f4b30db728682a348e23856
Billy Rhoades <billyarhoades@gmail.com>
2015-02-03 16:32:24 -0600

    Initial commit

---
+++ hw1/tool.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#.sh...submission guidelines on ercal's site. Sexsion B (V)
+
+function users
+{ 
+  w -h | cut -f1 -d' ' | sort -u 
+}
+
+while [[ "$SELECTION" != "exit" ]]
+do
+  PS3="Choose an option: "
+  [[ "$SELECTION" != "" ]] && printf "\n\n" # Space if this isn't our first run
+  select SELECTION in "Print Ancestry Tree" "List Users Online" "Get User's Processes" 'Exit'
+    do
+      case $SELECTION in 
+        "Print Ancestry Tree")
+          OUTPUT=$(pstree --ascii -ps $$) # Separate or it gets greedy and grabs sed
+          echo $OUTPUT | sed -e 's/---/\n|\n/g' | head -n -2 # Format it into a tree
+          break
+          ;;
+
+        "List Users Online")
+          echo Users Online:
+          echo $(users)
+          break
+          ;;
+
+        "Get User's Processes")
+          USERLIST=$(users)
+          PS3="Choose a user: " 
+          select USERNAME in $USERLIST
+          do
+            ps -u $USERNAME -U $USERNAME lax
+            break
+          done
+          break
+          ;;
+
+        'Exit')
+          break;;
+
+        * )
+          echo "Unknown Option"
+          break;;
+    esac
+  done
+done
